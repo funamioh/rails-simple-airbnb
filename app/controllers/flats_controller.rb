39582ffb0aff0df5f760a330d886c846b87b9e45
Miho Funayama <funamiho1011@gmail.com>
2024-01-12 16:01:55 +0900

    Adding redirect from Edit button in progress

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,49 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[ show ]
+  # get /flats
   def index
-    @flat = Flat.all
+    @flats = Flat.all
+  end
+
+  # get /flats/:id
+  def show
+  end
+
+  # get /flats/new
+  def new
+    @flat = Flat.new
+  end
+
+  # post /flats
+  def create
+    @flat = Flat.new(flat_param)
+
+    if @flat.save
+      redirect_to @flat, notice: "flat was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  # patch /flats/:id
+  def updated
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: "Flat was successfully updated!"
+    else
+      render :edit, status: :unproccessable_entity
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_param
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
 end
